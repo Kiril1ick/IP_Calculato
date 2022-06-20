a795128a97dec8678d1877ef6e7409ee4990fa40
k8906 <k8906@LAPTOP-J5ARLAUB>
2022-06-20 17:16:11 +0300

    Добавьте файлы проекта.

---
+++ IP_Calculato/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace IP_Calculato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(336, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(7, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hosts";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(7, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Network";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(7, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Brodcast";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(117, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 99);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(409, 520);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(425, 559);
+            this.MinimumSize = new System.Drawing.Size(425, 559);
+            this.Name = "Form1";
+            this.Text = "IP Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ IP_Calculato/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP_Calculato
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string FirstByte="", SecondByte=null, ThirdByte="", FouthByte="";
+            int ByteOne=0, ByteTwo=0, ByteThree=0, ByteFouth=0;
+            int Hosts;
+            int a=0,shag=0;
+            int Masc = Convert.ToInt32(textBox2.Text);
+            string txt1= textBox1.Text;
+            for (int i = 0; i < txt1.Length; i++)
+            {
+                if (txt1[i] == '.')
+                {
+                    a++;
+                }
+                if (a == 0 && txt1[i] != '.')
+                {
+                    FirstByte += txt1[i];
+                }
+                if (a == 1 && txt1[i]!='.')
+                {
+                    SecondByte += txt1[i];
+                }
+                if (a == 2 && txt1[i] != '.')
+                {
+                    ThirdByte += txt1[i];
+                }
+                if (a == 3 && txt1[i] != '.')
+                {
+                    FouthByte += txt1[i];
+                }
+            }
+
+            label1.Text = "IP =" + textBox1.Text;
+
+            Hosts = (int)Math.Pow(2, 32 - Masc);
+            label2.Text = "Hosts ="+ (Hosts-2);
+
+            for (int i=0;i<(32-Masc)%8;i++)
+            {
+                shag += (int)Math.Pow(2, i);
+            }
+
+            if (Masc>=0 && Masc<=8)
+            {
+                ByteOne = int.Parse(FirstByte) - shag;
+                if (ByteOne % (shag+1) != 0)
+                {
+                    while (ByteOne % (shag+1) != 0)
+                    {
+                        ByteOne++;
+                    }
+                }
+                label3.Text = "Network =" + ByteOne + "." + 0 + "." + 0 + "." + 0;
+            }
+            if (Masc >8 && Masc <= 16)
+            {
+                ByteTwo = int.Parse(SecondByte) - shag;
+                if (ByteTwo % (shag + 1) != 0)
+                {
+                    while (ByteTwo % (shag + 1) != 0)
+                    {
+                        ByteTwo++;
+                    }
+                }
+                label3.Text = "Network =" + FirstByte + "." + ByteTwo + "." + 0 + "." + 0;
+            }
+            if (Masc > 16 && Masc <= 24)
+            {
+                ByteThree = int.Parse(ThirdByte) - shag;
+                if (ByteThree % (shag + 1) != 0)
+                {
+                    while (ByteThree % (shag + 1) != 0)
+                    {
+                        ByteThree++;
+                    }
+                }
+                label3.Text = "Network =" + FirstByte + "." + SecondByte + "." + ByteThree + "." + 0;
+            }
+            if (Masc > 24 && Masc <= 32)
+            {
+                ByteFouth = int.Parse(FouthByte) - shag;
+                if (ByteFouth % (shag + 1) != 0)
+                {
+                    while (ByteFouth % (shag + 1) != 0)
+                    {
+                        ByteFouth++;
+                    }
+                }
+                label3.Text ="Network ="+ FirstByte + "." + SecondByte + "." + ThirdByte + "." + ByteFouth;
+            }
+
+
+            if (Masc >= 0 && Masc <= 8)
+            {
+                ByteOne +=shag;
+                label4.Text ="Brodecast ="+ ByteOne + "." + 255 + "." + 255 + "." + 255;
+            }
+            if (Masc > 8 && Masc <= 16)
+            {
+                ByteTwo +=shag;
+                label4.Text = "Brodecast =" + FirstByte + "." + ByteTwo + "." + 255 + "." + 255;
+            }
+            if (Masc > 16 && Masc <= 24)
+            {
+                ByteThree +=shag;
+                label4.Text = "Brodecast =" + FirstByte + "." + SecondByte + "." + ByteThree + "." + 255;
+            }
+            if (Masc > 24 && Masc <= 32)
+            {
+                ByteFouth +=shag;
+                label4.Text = "Brodecast =" + FirstByte + "." + SecondByte + "." + ThirdByte + "." + ByteFouth;
+            }
+        }
+    }
+}
